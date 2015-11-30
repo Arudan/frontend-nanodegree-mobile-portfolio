16a51d965d72f2c2dce63885b0bbcde743fa15c2
Alberto Francesco Motta <alberto.motta.86@gmail.com>
2015-12-01 00:15:22 +0100

    fix: removed inline-css plugin

--- gulp/optimize-html.js
+++ gulp/optimize-html.js
@@ -1,6 +1,5 @@
 var gulp    = require('gulp');
 var htmlmin = require('gulp-htmlmin');
-var inlineCss = require('gulp-inline-css');
 var config  = require('./config').optimize.html;
 var inlineConfig = require('./config').inlinecss;
 
